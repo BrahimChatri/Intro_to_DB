98d144cfc70f40f87d6921097e22741d096530b3
Brahim Chatri <brahim.chatri.dev@gmail.com>
2025-06-28 20:37:46 +0100

    fix errors

--- task_2.sql
+++ task_2.sql
@@ -16,10 +16,10 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS customer (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
 
